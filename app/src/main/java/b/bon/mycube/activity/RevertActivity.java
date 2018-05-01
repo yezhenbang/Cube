d73889f3e324b3bfb0d931c01a3d04e4cc471bf9
yezhenbang-yunba <yezhenbang@yunba.io>
2018-05-02 00:03:51 +0800

    'ini'

---
+++ app/src/main/java/b/bon/mycube/activity/RevertActivity.java
@@ -0,0 +1,17 @@
+package b.bon.mycube.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import b.bon.mycube.R;
+
+/**
+ * Created by Administrator on 2016/5/8.
+ */
+public class RevertActivity extends Activity {
+    @Override
+     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revert);
+    }
+}
---
+++ app/src/main/java/b/bon/mycube/model/ResultListener.java
@@ -0,0 +1,10 @@
+package b.bon.mycube.model;
+
+/**
+ * Created by ZhengQinyu on 2016/5/1.
+ */
+public interface ResultListener {
+
+    void onDataSuccessfully(Object data);
+    void onDataFailed();
+}
